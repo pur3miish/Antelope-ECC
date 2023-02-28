336b657f03175fe87c1dfae0f54e845fa400b717
H <pur3miish@me.com>
2023-02-28 11:41:22 +1000

    Removed JSdoc comment.

--- legacy_to_public_key.mjs
+++ legacy_to_public_key.mjs
@@ -4,8 +4,6 @@ import ripemd160 from "ripemd160-js/ripemd160.mjs";
 
 /**
  * Converts an EOSIO/Antelope legacy key to PUB_K1 format.
- * @name legacy_to_public_key
- * @kind function
  * @param {String} legacy legacy public key
  * @returns {Promise<String>} public key PUB_K1 format
  * @example <caption>Usage `legacy_to_public_key`.</caption>
